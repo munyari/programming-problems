29377c391dd3d57a9406a519c7ea1af300e7cff7
Panashe M. Fundira <fundirap@gmail.com>
2015-10-18 21:52:27 -0400

    add timus' own IO template

---
+++ ExampleIO.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class ExampleIO
+{
+  public static void main(String[] args) throws IOException
+  {
+    new ExampleIO.run();
+  }
+
+  StreamTokenizer in;
+  PrintWriter out;
+
+  int nextInt() throws IOException
+  {
+    in.nextToken();
+    return (int)in.nval;
+  }
+
+  void run() throws IOException
+  {
+    in = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+    out = new PrintWriter(new OutputStreamWriter(System.out));
+    solve();
+    out.flush();
+  }
+
+  void solve() throws IOException
+  {
+
+  }
+}
