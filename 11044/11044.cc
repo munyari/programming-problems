68c721e19a0bcfd2fe1720e8586f52b05fb01d2f
Panashe M. Fundira <fundirap@gmail.com>
2016-02-03 19:58:25 -0500

    11044

--- 11044/11044.cc
+++ 11044/11044.cc
@@ -1,5 +1,13 @@
 #include<iostream>
 
+int ceil(int dividend, int divisor)
+{
+  if (dividend % 3 == 0)
+    return dividend / 3;
+  else
+    return dividend / 3 + 1;
+}
+
 int main()
 {
   int t, n, m;
@@ -7,7 +15,8 @@ int main()
   while (t--)
   {
     scanf("%d %d", &n, &m);
-    printf("%d\n", ((n - 2) / 3 + 1) * ((m - 2) / 3 + 1));
+    printf("%d\n", ceil(n-2, 3) * ceil(m-2, 3));
   }
   return 0;
 }
+
