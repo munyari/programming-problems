a10083998f410952c1b6783e2a73d7b36da274c9
Panashe M. Fundira <fundirap@gmail.com>
2016-04-30 15:10:08 -0400

    Convert spec passing :thumbsup:
    refactored convert

--- convert/lib/convert.rb
+++ convert/lib/convert.rb
@@ -1,32 +1,31 @@
-require "pry"
-
 def convert(input, source, target)
-  acc = 0
-  # binding.pry
-  input.chars.each_with_index do |char, i|
-    acc += source.index(input[i]) * (source.length ** ((input.length - 1) - i))
-    # binding.pry
-  end
-  # binding.pry
-  res = ""
-
-  # binding.pry
-  pow_was_zero = false
-  until acc == 0 do
-    # highest power less than or equal to you
-    pow = Math.log(acc, target.length).to_i
-    pow_was_zero = pow == 0
-    # how many times does that go in
-    coeff = acc / (target.length ** pow)
-    acc -= coeff * (target.length ** pow)
-    res += "#{target[coeff]}"
-    # binding.pry
-  end
-  res += "#{target[0]}" unless pow_was_zero
-  res
-  # binding.pry
+  in_as_dec = convert_input_to_dec(input, source)
+  convert_dec_to_target(in_as_dec, target)
 end
 
-hex='0123456789abcdef'
-allow='abcdefghijklmnopqrstuvwxyz'
-convert("hello", allow, hex) #320048
+def convert_input_to_dec(input, source)
+  acc = 0
+  input.chars.each_with_index do |char, index|
+    acc += source.index(input[index]) * (source.length ** ((input.length - 1) - index))
+  end
+  acc
+end
+
+def convert_dec_to_target(input, target)
+  res = ""
+  target_base = target.length
+  if input != 0
+    # highest power less than or equal to you
+    highest_pow = Math.log(input, target_base).to_i
+    pow = highest_pow
+    until pow < 0 do
+      place_val = input / (target_base ** pow)
+      input -= place_val * (target_base ** pow)
+      res += "#{target[place_val]}"
+      pow -= 1
+    end
+    res
+  else
+    target[0]
+  end
+end
