15d37c243da47df2528c525b20d59d6a8ce4e366
Panashe M. Fundira <fundirap@gmail.com>
2016-06-08 23:35:02 -0400

    Exercism - [rust] #1 Hello, World!; iter 3

--- exercism.io/rust/hello-world/src/lib.rs
+++ exercism.io/rust/hello-world/src/lib.rs
@@ -1,6 +1,3 @@
 pub fn hello(name: Option<&str>) -> String {
-        match name {
-            Some(name) => "Hello, ".to_string() + &name.to_string() + "!",
-            None       => "Hello, World!".to_string()
-        }
+    format!("Hello, {}!", name.unwrap_or("World"))
 }
