73aa52772c2adff11d0eac83622865860a84bece
Panashe M. Fundira <fundirap@gmail.com>
2016-06-13 17:04:15 -0400

    Exercism - [ruby] #12 Leap: iter 2
    Use module instead of class

--- exercism.io/ruby/leap/leap.rb
+++ exercism.io/ruby/leap/leap.rb
@@ -1,6 +1,8 @@
-class Year
+module Year
   VERSION = 1
-  def self.leap?(year)
-    year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)
+  def leap?(year)
+    year % 400 == 0 || year % 4 == 0 && year % 100 != 0
   end
 end
+
+Year.extend(Year)
