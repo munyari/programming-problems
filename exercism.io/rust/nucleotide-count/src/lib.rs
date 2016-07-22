f2b7e984a95e7037b7ebf817eba24afa7975a70d
Panashe M. Fundira <fundirap@gmail.com>
2016-07-22 18:09:37 -0400

    Exercism - [rust] #11 Nucleotide Count: iter 2

--- exercism.io/rust/nucleotide-count/src/lib.rs
+++ exercism.io/rust/nucleotide-count/src/lib.rs
@@ -1,16 +1,9 @@
 use std::collections::HashMap;
 
+const NUCLEOTIDES: [char; 4] = ['A', 'T', 'C', 'G'];
+
 pub fn nucleotide_counts(dna_str: &str) -> HashMap<char, usize> {
-    let mut map: HashMap<char, usize> = HashMap::new();
-    map.insert('A', 0);
-    map.insert('T', 0);
-    map.insert('C', 0);
-    map.insert('G', 0);
-    for c in dna_str.chars() {
-        let val = map.entry(c).or_insert(0);
-        *val += 1;
-    }
-    map
+    NUCLEOTIDES.iter().map(|c| (*c, count(*c, dna_str))).collect()
 }
 
 pub fn count(nucleotide: char, dna_str: &str) -> usize {
