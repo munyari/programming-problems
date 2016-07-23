6ec7766955c8ed995d691ba7b5fe0e67054fb5b6
Panashe M. Fundira <fundirap@gmail.com>
2016-07-22 22:08:23 -0400

    Exercism - [rust] #13 Etl: iter 2

--- exercism.io/rust/etl/src/lib.rs
+++ exercism.io/rust/etl/src/lib.rs
@@ -1,17 +1,10 @@
 use std::collections::BTreeMap;
 
-const SCORES: [i32; 7] = [1,2,3,4,5,8,10];
-
 pub fn transform(input: &BTreeMap<i32, Vec<String>>) -> BTreeMap<String, i32> {
     let mut scrabble_map: BTreeMap<String, i32> = BTreeMap::new();
-    for score in SCORES.iter() {
-        match input.get(score) {
-            None => (),
-            Some(v) => {
-                for letter in v {
-                    scrabble_map.insert(letter.to_lowercase(), *score);
-                }
-            }
+    for (&score, letters) in input {
+        for letter in letters {
+            scrabble_map.insert(letter.to_lowercase(), score);
         }
     }
     scrabble_map
